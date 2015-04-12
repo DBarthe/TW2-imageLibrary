151501bcb6902e04f9fcc7d9a996cfc580110e20
Barthelemy Delemotte <barthelemy.delemotte@gmail.com>
2015-04-12 21:14:07 +0200

    config

---
+++ site/library/config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Configuration of the project.
+ * @var array
+ */
+$CONFIG = array(
+
+  'DATABASE' => array(
+    'host' => 'localhost',
+    'port' => 5432,
+    'database' => 'projet_tw2',
+    'user' => 'barth',
+    'password' => 'ne5uiJ4h'
+  )
+
+);
+
+?>
+
