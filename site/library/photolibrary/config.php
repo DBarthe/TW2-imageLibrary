41f63fc983374a109bf3e46c8c3ec531e3f517c5
Barthelemy Delemotte <barthelemy.delemotte@gmail.com>
2015-04-15 18:50:22 +0200

    config

--- site/library/photolibrary/config.php
+++ site/library/photolibrary/config.php
@@ -1,20 +1,19 @@
 <?php
+namespace PhotoLibrary;
 
-/**
- * Configuration of the project.
- * @var array
- */
-$CONFIG = array(
+class Config {
 
-  'DATABASE' => array(
+  public static $DATABASE = array(
     'host' => 'localhost',
     'port' => 5432,
     'database' => 'projet_tw2',
     'user' => 'barth',
     'password' => 'ne5uiJ4h'
-  )
+  );
 
-);
+  public static $THUMBNAILS_DIR = './thumbnails'; // relative to the SITE ROOT
+  public static $THUMBNAILS_EXT = 'png';
+}
 
 ?>
 
