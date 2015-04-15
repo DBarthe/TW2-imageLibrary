43e0fa9c28566d4ff665e43f04db59cf48280beb
Barthelemy Delemotte <barthelemy.delemotte@gmail.com>
2015-04-15 18:50:30 +0200

    shortcuts

---
+++ site/library/photolibrary/shortcuts.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace PhotoLibrary;
+
+use \PhotoLibrary\Database;
+
+class Shortcuts {
+
+  /**
+   * Create the database gateway and return it.
+   * If this function is called multiple times, the same object is always returned.
+   * @return DatabaseGateway the database gateway
+   */
+  public static function getDatabase(){
+    static $db = null;
+
+    if (is_null($db)){
+      $adapter = new Database\PgsqlAdapter(\PhotoLibrary\Config::$DATABASE);
+      $db = new Database\DatabaseGateway($adapter);
+    }
+    return $db;
+  }
+
+  /**
+   * Giving an image row, return its thumbnail filename (without path).
+   */
+  function getThumbnailName($imageRow){
+    return $imageRow['id'].'.'.\PhotoLibrary\Config::$THUMBNAILS_EXT;
+  }
+}
+
+?>
